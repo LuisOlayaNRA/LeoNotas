72a9f89416705797243edcde4b0487dd3a670788
Luis <luis.olaya@numrot.com>
2024-09-08 22:11:38 -0500

    ajuste a parciales desde development

--- CICDNotas/Models/Parcial.cs
+++ CICDNotas/Models/Parcial.cs
@@ -3,7 +3,8 @@
 public class Parcial
 {
     public int Id { get; set; }
-    public string Nombre { get; set; }
+    public string Nombres { get; set; }
+    public string Profes { get; set; }
     public int Nota { get; set; }
     public int EstudianteId { get; set; }
     public Estudiante Estudiante { get; set; }
