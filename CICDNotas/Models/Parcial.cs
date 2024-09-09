a1956eca73b36d151e21ae4391dd2ad308910927
Luis <luis.olaya@numrot.com>
2024-09-08 21:33:39 -0500

    Se agrega modelo de Parcial

---
+++ CICDNotas/Models/Parcial.cs
@@ -0,0 +1,10 @@
+﻿namespace CICDNotas.Models;
+
+public class Parcial
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Nota { get; set; }
+    public int EstudianteId { get; set; }
+    public Estudiante Estudiante { get; set; }
+}
