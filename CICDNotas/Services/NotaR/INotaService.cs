c0fe560fd2e309e2503422f83fd29fcfb250f310
LuisOlayaNRA <66016550+LuisOlayaNRA@users.noreply.github.com>
2024-09-18 21:34:46 -0500

    Se ajusta notas interface (#7)

--- CICDNotas/Services/NotaR/INotaService.cs
+++ CICDNotas/Services/NotaR/INotaService.cs
@@ -5,9 +5,10 @@ namespace RegistroNotas.Services.NotaR
 {
   public interface INotaService
   {
-    //public IEnumerable<Nota> GetMostrarNota();
-    //public Task Save(Nota nota);
-    //public List<Nota> TodasLasNotas(int Id_Estudiante, int Id_Materia);
-    //public string CalcularNota(int Id_Estudiante, int Id_Materia);
+      public IEnumerable<Nota> GetMostrarNota();
+      public Task Save(Nota nota);
+      public List<Nota> TodasLasNotas(int Id_Estudiante, int Id_Materia);
+     public string CalcularNota(int Id_Estudiante, int Id_Materia);
+
   }
 }
