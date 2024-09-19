5ab8b84c1da6c82a32de0c5723a7a1c66b078fe2
Luis <luis.olaya@numrot.com>
2024-09-18 20:33:11 -0500

    Se crea modelo profesor

---
+++ CICDNotas/Models/Profesor.cs
@@ -0,0 +1,9 @@
+﻿namespace CICDNotas.Models
+{
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+    }
+}
