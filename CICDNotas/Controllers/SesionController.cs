d6e09d93bdfd7b81a12cf821f03956b92df46dca
LuisOlayaNRA <66016550+LuisOlayaNRA@users.noreply.github.com>
2024-09-18 20:47:27 -0500

    Dllo sesion (#5)
    * Se crea controlador de la sesion
    
    * Se crea sesion de la interface
    
    * Se agregan metodos en la interface

---
+++ CICDNotas/Controllers/SesionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CICDNotas.Controllers
+{
+    public class SesionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
